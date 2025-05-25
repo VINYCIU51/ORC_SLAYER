fdbf18c2a92e187509adfc5fc4844b0c608da4a5
vinycius <vinyciusalves155@gmail.com>
2025-05-24 23:28:17 -0300

    adicionado o tiro de flecha

--- actors/script/player.gd
+++ actors/script/player.gd
@@ -1,11 +1,9 @@
 extends CharacterBody2D
 
+const ARROW := preload("res://actors/scenes/projectiles/arrow.tscn")
 const SPEED = 400.0
 const JUMP_VELOCITY = -400.0
-const DEATH_HEIGHT = 1400.0
-
-@onready var animation: AnimationPlayer = $animation
-@onready var sprite: Sprite2D = $sprite
+const DEATH_HEIGHT = 1000.0
 
 var direction = Vector2.ZERO
 var is_attacking = false
@@ -18,7 +16,7 @@ func _physics_process(delta):
 	
 	if is_attacking:
 		# IMPEDE QUE OUTRAS ANIMAÇÕES ATRAPALHEM AS DE ATAQUE
-		if not animation.is_playing():
+		if not $animation.is_playing():
 			is_attacking = false
 		move_and_slide()
 		return
@@ -28,20 +26,28 @@ func _physics_process(delta):
 		velocity.y += get_gravity().y * delta
 	else:
 		if velocity.x == 0: 
-			animation.play("idle")
+			$animation.play("idle")
 	
 # EFETUA O SALTO
 	if Input.is_key_pressed(KEY_SPACE) and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-		animation.play("jump")    
+		$animation.play("jump")    
 
 # EFETUA O MOVIMENTO DO PERSONAGEM
 	if Input.is_key_pressed(KEY_D):
 		direction.x = 1 
-		sprite.flip_h = false
+		$sprite.flip_h = false
+		
+		if sign($arrow_point.position.x) == -1:
+			$arrow_point.position.x *= -1
+		
 	elif Input.is_key_pressed(KEY_A): 
 		direction.x = -1
-		sprite.flip_h = true
+		$sprite.flip_h = true
+		
+		if sign($arrow_point.position.x) == 1:
+			$arrow_point.position.x *= -1
+		
 	else:
 		direction.x = 0
 
@@ -50,20 +56,38 @@ func _physics_process(delta):
 
 # APLICA A ANIMAÇÃO DE CAMINHAR
 	if is_on_floor() and direction.x != 0:
-		animation.play("walk")
+		$animation.play("walk")
 
-# EFETUA OS ATAQUES DE ESPADA E FLECHA
+# EFETUA OS ATAQUES DE ESPADA
 	if is_on_floor() and Input.is_action_just_pressed("left_click"):
 		velocity.x = 0
 		is_attacking = true
-		animation.play("atack")
-		
+		$animation.play("atack")
+
+# EFETUA OS ATAQUES DE FLECHA
 	if is_on_floor() and Input.is_action_just_pressed("right_click"):
-		velocity.x = 0
-		is_attacking = true
-		animation.play("arrow")
+		
+#		ATIRA AS FLECHAS INFORMANDO A DIREÇAO CORRETA
+		if sign($arrow_point.position.x) == 1:
+			shoot_arrow(1)
+		else:
+			shoot_arrow(-1)
+		
+		velocity.x = 0 # PARA O PLAYER
+		is_attacking = true # IMPEDE INTERRUPÇOES
+		$animation.play("arrow") # CHAMA A ANIMAÇAO
 
 	move_and_slide()
 
+# FUNÇAO DE MORTE
 func die():
-	get_tree().reload_current_scene()
+	get_tree().reload_current_scene() # RECARREGA O JOGO DO INICIO
+
+# FUNÇAO PARA ATIRAR FLECHAS
+func shoot_arrow(direct):
+	await get_tree().create_timer(0.5).timeout # ESPERA UM TEMPO PARA SINCRONIZAR COM A ANIMACAO
+	
+	var arrow_instance = ARROW.instantiate() # CRIA A INSTANCIA DA FLECHA
+	get_parent().add_child(arrow_instance) # FAZ ELA APARECER NA TELA DO MUNDO
+	arrow_instance.set_direction(direct) # DEFINE PARA QUE LADO A FLECHA VAI
+	arrow_instance.position = $arrow_point.global_position # FAZ ELA SAIR DA MAO DO PLAYER
