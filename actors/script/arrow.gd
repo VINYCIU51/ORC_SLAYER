0f3f783ddba211ec0557417bf33288a307da2140
vinycius <vinyciusalves155@gmail.com>
2025-05-24 23:31:38 -0300

    ajuste dos diretorios

---
+++ actors/script/arrow.gd
@@ -0,0 +1,25 @@
+extends Area2D
+
+const SPEED := 280
+
+var velocity := Vector2.ZERO
+var direction := 1
+
+func _ready() -> void:
+	pass
+
+# DEFINE A DIREÇAO DA FLECHA
+func set_direction(direct):
+	direction = direct
+	if direction == 1:
+		$sprite.flip_h = false
+	else:
+		$sprite.flip_h = true
+
+# FAZ ELA SE MOVER
+func _physics_process(delta: float) -> void:
+	position.x += SPEED * delta * direction
+
+# FAZ ELA SUMIR AO SAIR DA TELA
+func _on_visibility_screen_exited() -> void:
+	queue_free()
