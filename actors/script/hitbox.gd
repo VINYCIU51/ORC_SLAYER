07e105628c27f4f030974bf89560020037786ca6
vinycius <vinyciusalves155@gmail.com>
2025-05-25 23:28:52 -0300

    ajustes para evitar colisões erronhas

--- actors/script/hitbox.gd
+++ actors/script/hitbox.gd
@@ -2,9 +2,7 @@ extends Area2D
 
 
 func _on_body_entered(body: Node2D) -> void:
-	if body.name == "player":
-		body.take_damage()
 	
 	if body.name == "arrow":
 		body.queue_free()
-		owner.animation.play("die")
+		owner.take_damage()
--- actors/script/hurtbox.gd
+++ actors/script/hurtbox.gd
@@ -1,7 +1,5 @@
 extends Area2D
 
-
-
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("enemies"):
-		owner.animation.play("death")
+		owner.take_damage()
