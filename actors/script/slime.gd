eb7494d487832e3336893f08121ccb4980f79481
vinycius <vinyciusalves155@gmail.com>
2025-05-31 00:27:58 -0300

    ajuste das animaçoes do slime

--- actors/script/slime.gd
+++ actors/script/slime.gd
@@ -17,7 +17,7 @@ func _physics_process(delta: float) -> void:
 
 	if wall_detection.is_colliding():
 		direction *= -1
-		$sprite.flip_h = false if direction == -1 else true
+		$sprite.flip_h = direction > 0
 		wall_detection.target_position.x = direction * abs(wall_detection.target_position.x)
 		
 	velocity.x = direction * SPEED
@@ -28,6 +28,9 @@ func _physics_process(delta: float) -> void:
 			taked_damage = false
 		move_and_slide()
 		return
+		
+	if direction != 0:
+		animation.play("walk")
 
 	move_and_slide()
 	
