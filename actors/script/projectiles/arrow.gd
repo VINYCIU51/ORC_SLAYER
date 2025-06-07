640fb0f0f875931218f69e5c9bd9136266117fca
vinycius <vinyciusalves155@gmail.com>
2025-06-07 15:32:32 -0300

    correçao do bug de flechass fincadas dando dano

--- actors/script/projectiles/arrow.gd
+++ actors/script/projectiles/arrow.gd
@@ -1,27 +1,27 @@
+class_name Arrow
 extends CharacterBody2D
 
 @onready var wall_collision := $wall_collision as RayCast2D
 
 const SPEED := 300
-const DAMAGE := 1
 const ARROW_DURATION := 3.5
 
+var has_collided := false
+var damage := 1
 var direction := 1
 
-# Sets the arrow direction
-func set_direction(dir):
-	direction = dir
-	$sprite.flip_h = direction < 0
-	$wall_collision.target_position.x = $wall_collision.target_position.x * direction
 
-# Makes it move
 func _physics_process(delta: float) -> void:
-	velocity.x = SPEED * direction
-	move_and_slide()
-	
-	if wall_collision.is_colliding():
-		stop_and_disappear()
-	
+	if !has_collided:
+		velocity.x = SPEED * direction
+		move_and_slide()
+		
+		if wall_collision.is_colliding():
+			has_collided = true
+			damage = 0
+			stop_and_disappear()
+
+# faz a flecha parar e desaparecer depois de um tempo ao colidir com a parede
 func stop_and_disappear():
 	velocity.x = 0
 	velocity.y = 0
@@ -29,6 +29,12 @@ func stop_and_disappear():
 	await get_tree().create_timer(ARROW_DURATION).timeout
 	queue_free()
 
-# Makes it disappear when it leaves the screen
+# ajusta a direcao da flecha
+func set_direction(dir):
+	direction = dir
+	$sprite.flip_h = direction < 0
+	$wall_collision.target_position.x = $wall_collision.target_position.x * direction
+
+# faz a flecha desaparecer ao sair da tela
 func _on_visibility_screen_exited() -> void:
 	queue_free()
