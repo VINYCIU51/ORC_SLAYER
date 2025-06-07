a01864aea9fd14421b2da814a934a3c563edbb2e
vinycius <vinyciusalves155@gmail.com>
2025-06-07 11:26:49 -0300

    melhorias do player para melhor funcionamento

--- actors/script/player/player.gd
+++ actors/script/player/player.gd
@@ -1,8 +1,10 @@
-extends CharacterBody2D
+class_name Player
+extends CharacterBody2D 
 
-@onready var animation := $animation as AnimationPlayer
-@onready var invencible_timer := $invencible_timer
+@onready var animation := $body/animation
+@onready var invincible_timer := $invincible_timer
 @onready var blink_timer := $blink_timer
+@onready var sprite := $body/sprite
 
 const ARROW := preload("res://actors/scenes/projectiles/arrow.tscn")
 const SPEED := 200
@@ -20,18 +22,21 @@ var fall_gravity
 
 var direction = 0.0
 
-var is_invencible := false
-var taked_damage := false
+var is_invincible := false
+var took_damage := false
 var is_attacking := false
 var is_shooting := false
 var is_air_shooting := false
-var is_parrying := false
+var has_parried := false
+var is_blocking := false
 var current_state := "idle"
 
 func _ready():
 	jump_velocity = (jump_height * 2) / time_to_top_height
 	gravity = (jump_height * 2) / pow(time_to_top_height, 2)
 	fall_gravity = gravity * 2
+	
+	$parry/parry_area.disabled = true
 
 func _physics_process(delta):
 	if global_position.y > DEATH_HEIGHT:
@@ -44,14 +49,14 @@ func _physics_process(delta):
 
 	direction = Input.get_axis("move_left", "move_right")
 
-	if !taked_damage:
+	if !took_damage:
 		velocity.x = direction * SPEED
 
-	if direction != 0 and !is_attacking and !is_shooting and !is_air_shooting:
-		rotate_player(direction)
+	if direction != 0 and !is_attacking and !is_shooting and !is_air_shooting and !is_blocking:
+		flip_sprite(direction)
 
 	# Pulo
-	if Input.is_action_just_pressed("jump") and is_on_floor() and !is_attacking:
+	if Input.is_action_just_pressed("jump") and is_on_floor() and !is_attacking and !is_blocking:
 		velocity.y = -jump_velocity
 
 	# Gravidade
@@ -62,25 +67,24 @@ func _physics_process(delta):
 			velocity.y += fall_gravity * delta
 
 	# Tiro no ar
-	if !is_on_floor() and Input.is_action_just_pressed("right_click") and !is_attacking and !is_air_shooting:
+	if !is_on_floor() and Input.is_action_just_pressed("interact") and !is_attacking and !is_air_shooting:
 		is_air_shooting = true
-		shoot_arrow(sign($arrow_point.position.x), 0.3)
 
 	# Tiro no chão
-	if is_on_floor() and Input.is_action_just_pressed("right_click") and !is_shooting and !is_attacking:
+	if is_on_floor() and Input.is_action_just_pressed("interact") and !is_shooting and !is_attacking and !is_blocking:
 		is_shooting = true
-		shoot_arrow(sign($arrow_point.position.x))
 
 	# Ataque corpo-a-corpo
-	if is_on_floor() and Input.is_action_just_pressed("left_click") and !is_attacking and !is_shooting:
+	if is_on_floor() and Input.is_action_just_pressed("left_click") and !is_attacking and !is_shooting and !is_blocking:
 		is_attacking = true
 
-	if Input.is_action_just_pressed("interact"):
-		is_parrying = true
+	if is_on_floor() and Input.is_action_just_pressed("right_click") and !is_attacking and !is_shooting and !is_air_shooting:
+		is_blocking = true
 		
-	if is_parrying:
+	set_state()
+	if is_blocking:
 		velocity.x = 0
-		if !animation.is_playing(): is_parrying = false
+		if !animation.is_playing() or current_state != "parry": is_blocking = false
 
 	# Verifica se terminou o ataque
 	if is_attacking:
@@ -92,24 +96,23 @@ func _physics_process(delta):
 		velocity.x = 0
 		if !animation.is_playing(): is_shooting = false
 	
-	# Verufica o fim do tiro aereo
+	# Verifica o fim do tiro aéreo
 	if is_air_shooting:
 		if !animation.is_playing(): is_air_shooting = false
 
 	# Verifica fim de dano
-	if taked_damage:
+	if took_damage:
 		knockback()
-		if !animation.is_playing(): taked_damage = false
+		if !animation.is_playing(): took_damage = false
 
 	move_and_slide()
-	set_state()
 
 func set_state():
 	var new_state = "idle"
 
 	if is_dead:
 		new_state = "die"
-	elif taked_damage:
+	elif took_damage:
 		new_state = "hurt"
 	elif is_attacking:
 		new_state = "attack"
@@ -117,7 +120,7 @@ func set_state():
 		new_state = "arrow"
 	elif is_air_shooting:
 		new_state = "air_arrow"
-	elif is_parrying:
+	elif is_blocking:
 		new_state = "parry"
 	elif !is_on_floor():
 		new_state = "jump"
@@ -131,46 +134,41 @@ func set_state():
 func fall_out():
 	get_tree().reload_current_scene()
 
-func take_damage(damage = 1):
-	if is_invencible or is_dead: return
+func take_damage(damage := 1):
+	if is_invincible or is_dead or has_parried: return
 
-	taked_damage = true
-	invencible_mode()
+	took_damage = true
+	invincible_mode()
 	life -= damage
 
 	if life <= 0: is_dead = true
 
-func shoot_arrow(direct, time := 0.5):
-	await get_tree().create_timer(time).timeout
+func shoot_arrow():
 	var arrow_instance = ARROW.instantiate()
 	add_sibling(arrow_instance, true)
+	
+	var direct = sign($body.scale.x)
 	arrow_instance.set_direction(direct)
-	arrow_instance.position = $arrow_point.global_position
+	arrow_instance.position = $body/arrow_point.global_position
 
-func rotate_player(direction):
-	$sprite.flip_h = direction < 0
-	$arrow_point.position.x = abs($arrow_point.position.x) * direction
-	$melee.position.x = abs($melee.position.x) * direction
+func flip_sprite(dir):
+	$body.scale.x = sign(dir)
 
 func knockback():
-	var knock_direction = 1 if $sprite.flip_h else -1
+	var knock_direction = -sign($body.scale.x)
 	velocity.x = knock_direction * 100
 
-func _on_melee_body_entered(body: Node2D):
-	if body.is_in_group("enemies"):
-		body.take_damage(SWORD_DAMAGE)
-		
-func invencible_mode():
-	is_invencible = true
+func invincible_mode():
+	is_invincible = true
 	set_collision_mask_value(3,false)
 	blink_timer.start()
-	invencible_timer.start()
+	invincible_timer.start()
 
-func _on_invencible_timer_timeout() -> void:
-	is_invencible = false
+func _on_invincible_timer_timeout() -> void:
+	is_invincible = false
 	set_collision_mask_value(3,true)
 	blink_timer.stop()
-	$sprite.visible = true
+	sprite.visible = true
 
 func _on_blink_timer_timeout() -> void:
-	$sprite.visible = !$sprite.visible
+	sprite.visible = !sprite.visible
