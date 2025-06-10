556af653a7c327ec1f3191cc3fbb96d988eaf8fa
vinycius <vinyciusalves155@gmail.com>
2025-06-09 22:00:24 -0300

    pequena correcao de erros de colisao

--- actors/script/enemies/goblin_smasher/goblin_smasher_atk.gd
+++ actors/script/enemies/goblin_smasher/goblin_smasher_atk.gd
@@ -1,14 +1,26 @@
 extends Area2D
 
-var mob = Goblin_smasher
-
 func _on_body_entered(body: Node2D) -> void:
+	var mob = get_mob()
+	
 	# se o player tiver entrado em contato com a area, ele toma o dano referente á força do mob
 	if body.name == "player":
 		body.take_damage(mob.DAMAGE)
 		
 
 func _on_area_entered(area: Area2D) -> void:
+	var mob = get_mob()
+	
 	# se  player estiver com o parry ativo no momento do impacto, o mob recebe um parry
 	if area.name == "parry":
 		mob.has_parried = true
+
+
+# encontra o nó raiz ou o nó que pertence à classe especificada
+func get_mob():
+	var current_node = self
+	while current_node:
+		if current_node is Goblin_smasher:
+			return current_node
+		current_node = current_node.get_parent()
+	return null
