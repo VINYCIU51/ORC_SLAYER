1756a8614bf19ee49ea1226a7e775f54405c10c6
vinycius <vinyciusalves155@gmail.com>
2025-06-14 00:37:46 -0300

    ajuste no player para nao levar dano do boneco de treino

--- script/player/player_hurtbox.gd
+++ script/player/player_hurtbox.gd
@@ -6,6 +6,7 @@ func _on_body_entered(body: Node2D) -> void:
 	
 	# aplica dano ao player por tocar em um inimigo
 	if body.is_in_group("enemies") or body.is_in_group("enemies_projectiles"):
-		if body.name == "training_dummy": return
+		# Evita o hit ao tocar em um boneco de treino
+		if body.name.begins_with("training_dummy"): return
 		
 		player.take_damage(1, body.global_position)
