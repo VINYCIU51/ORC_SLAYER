9eb94f9edd7d559bf41dfbff00fc6fbb1e556d01
vinycius <vinyciusalves155@gmail.com>
2025-06-13 23:53:42 -0300

    ajuste de diretorios

--- actors/script/player/player_atk.gd
+++
@@ -1,9 +0,0 @@
-extends Area2D
-
-@export var player : Player
-
-func _on_body_entered(body: Node2D) -> void:
-	
-	# aplica o dano ao inimigo 
-	if body.is_in_group("enemies"):
-		body.take_damage(player.SWORD_DAMAGE)
--- actors/script/player/player_hurtbox.gd
+++
@@ -1,9 +0,0 @@
-extends Area2D
-
-@export var player : Player
-
-func _on_body_entered(body: Node2D) -> void:
-	
-	# aplica dano ao player por tocar em um inimigo
-	if body.is_in_group("enemies") or body.is_in_group("enemies_projectiles"):
-		player.take_damage(1, body.global_position)
