ce94b3a079a9b9812fba9c3bac128ba4af6817c5
vinycius <vinyciusalves155@gmail.com>
2025-06-22 23:39:35 -0300

    utilizacao de scripts embutidos

--- script/worlds/cave/cave_entrance.gd
+++
@@ -1,5 +0,0 @@
-extends Node2D
-
-func _ready():
-	LevelsGen.parts_to_create = randi_range(LevelsGen.min_level_parts, LevelsGen.max_level_parts)
-	LevelsGen.generate_parts("res://scenes/worlds/cave_map/cave_parts/cave_part_",self , $fusion_point.position)
--- script/worlds/cave/generate_parts.gd
+++
@@ -1,5 +0,0 @@
-extends Node2D
-
-func _ready():
-	await get_tree().create_timer(0.5).timeout
-	LevelsGen.generate_parts("res://scenes/worlds/cave_map/cave_parts/cave_part_",self , $fusion_point.position)
