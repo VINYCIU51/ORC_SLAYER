53a9d82382c1707b94121c366979ae5e3f94df1b
vinycius <vinyciusalves155@gmail.com>
2025-06-29 23:58:32 -0300

    correçao da geraçao da sala do boss

--- script/globals/LevelsGen.gd
+++ script/globals/LevelsGen.gd
@@ -1,7 +1,7 @@
 extends Node
 
-var max_level_parts := 3
-var min_level_parts := 3
+var max_level_parts := 2
+var min_level_parts := 2
 var parts_to_create := 0
 
 func generate_parts(path: String, parent : Node ,position : Vector2):
@@ -14,13 +14,13 @@ func generate_parts(path: String, parent : Node ,position : Vector2):
 		parent.add_child(part)
 		parts_to_create -= 1
 		
-	if parts_to_create <= 0:
+	elif parts_to_create < 1:
 		var part = load(path + "boss.tscn").instantiate()
 		part.position = position
 		parent.add_child(part)
 
 func repeat_part (path: String, parent : Node ,position : Vector2):
-	if parts_to_create >= 1:
+	if parts_to_create > 0:
 		var part = load(path).instantiate()
 		part.position = position
 		parent.add_child(part)
