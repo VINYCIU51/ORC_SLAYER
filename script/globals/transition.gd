a598efe936abf215a91d8f0edbe1a511eb2fcef3
vinycius <vinyciusalves155@gmail.com>
2025-07-02 11:42:35 -0300

    commits de codigo

--- script/globals/transition.gd
+++ script/globals/transition.gd
@@ -1,5 +1,6 @@
 extends Node
 
+# Dicionario para guardar os caminhos das transicoes
 var scenes : Dictionary = {"menu":"res://scenes/interface/main_menu.tscn",
 						  "tutorial":"res://scenes/worlds/tutorial.tscn",
 						  "forest":"res://scenes/worlds/forest.tscn",
@@ -10,10 +11,12 @@ func to(level : String):
 	var scene_path :String = scenes.get(level)
 	
 	if scene_path != null:
+		PauseMenu.pause_enabled = false # desativa o pause durante a transicao
 		await LoadingScreen.start()
 		
-		get_tree().change_scene_to_file(scene_path)
+		get_tree().change_scene_to_file(scene_path) # faz a transicao no momento em que a tela esta escura
 
 		await get_tree().process_frame
 		
 		await LoadingScreen.end()
+		PauseMenu.pause_enabled = true # reativa o pause
