c3773c4d39830a0aff2d4d1971d1f54fa5f3be00
vinycius <vinyciusalves155@gmail.com>
2025-07-07 23:49:56 -0300

    pequenos ajustes

--- script/enemies/boss_forest.gd
+++ script/enemies/boss_forest.gd
@@ -11,19 +11,5 @@ func _ready() -> void:
 	max_parry_resistance = 15
 	dist_follow = 350
 	dist_mellee = 55
-	should_jump = true
 	flip_compensation = -36
 	position_compensation = 36
-
-#func _physics_process(delta: float) -> void:
-	#super._physics_process(delta)
-	#
-	#var distance = Mobs.distance_to(self, player)
-	#
-	#if distance > dist_follow:
-		#var attack_range = randi_range(1, 2)
-		#
-		#if attack_range == 1:
-			#animation.play("attack_2")
-		#else:
-			#animation.play("teleport")
